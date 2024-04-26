67e401c546bb7adcc20939da826742a2b0bba87e
ahmedfathy0-0 <ahmed@DESKTOP-ASV0IA0>
2024-04-27 00:18:23 +0300

    SubBytes is done with its testbench

---
+++ SBox_Test.v
@@ -0,0 +1,22 @@
+module SBox_Test();
+wire [7:0] out;
+reg [7:0] in;
+SBox SBox(in,out);
+
+initial begin
+in = 8'hee;
+#10;
+in = 8'h01;
+#10;
+in = 8'hff;
+#10;
+in = 8'h03;
+#10;
+in = 8'h1f;
+#10;
+end
+
+initial begin
+$monitor("In=%h, Out=%h",in,out);
+end
+endmodule
