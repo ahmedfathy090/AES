471720c06c6961fdac81a2a2f345f3213a5620f4
Habiba Ayman <HabibaAyman2004@outlook.com>
2024-04-28 16:25:59 +0300

    :thinking: refactored the whole module

--- Cipher.v
+++ Cipher.v
@@ -1,13 +1,24 @@
-module Cipher #(parameter Nk=4,parameter Nr=10) (input [127:0] plainText, output [127:0] encryptedText, input [Nk*32-1:0] key); 
+module Cipher #(parameter Nk=4,parameter Nr = Nk + 6) (plainText, keys, encryptedText); 
 
-reg [127:0] state;
-state = plainText;
-wire [127:0] AR_key;
+// Main module parameters
+input [127:0] plainText;
+input [(Nk*32) * Nr - 1:0] keys; // whole keys
+output [127:0] encryptedText;
 
-//AddRoundKey #(Nk,Nr) ARK (plainText, key, AR_key);
+wire [127:0] state [0:Nr]; // array of (128bit wire) of size Nr
+state[0] = plainText; // initial round
+
+AddRoundKey #(Nk,Nr) ARK (state[0], key[(Nk*32) * Nr - 1 -:128], state[1]);
 genvar i;
-for (i=0; i< Nr - 1; i=i+1) begin
-Round #(Nk,Nr) R1 (state, key, state);
+for (i = 1; i < Nr; i = i - 1) begin
+Round #(Nk,Nr) encryptionRound (state[i - 1], keys[128*i-:128], state[i]);
 end
-EncryptedText=State;
+assign SB_IN = state[Nr - 1];
+
+// Final round
+SubBytes S_B (SB_IN, SB_OUT);
+shift_rows SR(SB_OUT, SR_OUT);
+MixColumns MC(SR_OUT,MC_OUT);
+
+assign encryptedText = MC_OUT;
 endmodule
