a5746d0424e51479e4e95119b2f67e516595740c
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2024-04-28 16:30:27 +0300

    :bug: bug fixes

--- Cipher.v
+++ Cipher.v
@@ -5,18 +5,22 @@ input [127:0] plainText;
 input [(Nk*32) * Nr - 1:0] keys; // whole keys
 output [127:0] encryptedText;
 
+// temp parameters
+wire SB_IN, SB_OUT, SR_OUT, MC_OUT;
 wire [127:0] state [0:Nr]; // array of (128bit wire) of size Nr
-state[0] = plainText; // initial round
 
-AddRoundKey #(Nk,Nr) ARK (state[0], key[(Nk*32) * Nr - 1 -:128], state[1]);
+// initial round 
+AddRoundKey ARK (plainText, key[0+:128], state[0]);
+
+// Loop through all rounds (excluding final round)
 genvar i;
-for (i = 1; i < Nr; i = i - 1) begin
-Round #(Nk,Nr) encryptionRound (state[i - 1], keys[128*i-:128], state[i]);
+for (i = 1; i < Nr; i = i + 1) begin 
+Round #(Nk,Nr) encryptionRound (state[i - 1], keys[128*i+:128], state[i]);
 end
 assign SB_IN = state[Nr - 1];
 
 // Final round
-SubBytes S_B (SB_IN, SB_OUT);
+SubBytes SB (SB_IN, SB_OUT);
 shift_rows SR(SB_OUT, SR_OUT);
 MixColumns MC(SR_OUT,MC_OUT);
 
