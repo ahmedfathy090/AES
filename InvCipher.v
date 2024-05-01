ab70a770d79a656e3d33a6b8a873b4ecf376671d
ahmedfathy0-0 <ahmed@DESKTOP-ASV0IA0>
2024-05-02 02:26:51 +0300

    edit Cipher and InvCipher

--- InvCipher.v
+++ InvCipher.v
@@ -1,9 +1,11 @@
-module InvCipher #(parameter Nk=4,parameter Nr = Nk + 6) (encryptedText, keys, decryptedText); 
+module InvCipher #(parameter Nk=4,parameter Nr = Nk + 6) (encryptedText, keys, decryptedText, roundOutreg); 
 
 // Main module parameters
 input [127:0] encryptedText;
 input [(Nk*32) * Nr - 1:0] keys; // whole keys
 output [127:0] decryptedText;
+
+output reg [0:(Nr+1)*128-1] roundOutreg;
 
 // temp parameters
 wire [127:0] Inv_SB_IN, Inv_SB_OUT, Inv_SR_OUT, Inv_AR_OUT;
@@ -11,7 +13,8 @@ wire [127:0] state [0:Nr]; // array of (128bit wire) of size Nr
 
 // initial round 
 AddRoundKey ARK (encryptedText, keys[0+:128], state[0]);
-
+initial
+roundOutreg[0:128] = state[0];
 // Loop through all rounds (excluding final round)
 genvar i;
 generate
@@ -24,7 +27,15 @@ assign Inv_SB_IN = state[Nr - 1];
 // Final round
 InvSubBytes SB (Inv_SB_IN, Inv_SB_OUT);
 Invshift_rows SR(Inv_SB_OUT, Inv_SR_OUT);
-AddRoundKey ARK1 (Inv_SR_OUT, keys[(Nr)*128+:128], Inv_AR_OUT); 
+AddRoundKey ARK1 (Inv_SR_OUT, keys[(Nr-1)*128+:128], Inv_AR_OUT); 
 
 assign decryptedText = Inv_AR_OUT;
+
+integer j;
+// Store all the states to be printed
+initial begin
+    for (j = 0; j <= Nr; j = j + 1) begin
+        roundOutreg[128*j+:128] = state[j];
+    end
+end
 endmodule
--- Rounds/InvRound.v
+++ Rounds/InvRound.v
@@ -2,18 +2,19 @@ module InvRound #(parameter Nk=4,parameter Nr=10) (state, key, InvRoundOut);
 
 // Main module parameters
 input [127:0] state;
-input [32*Nk -1: 0] key;
+input [32*Nk-1: 0] key;
 output [127:0] InvRoundOut;
 
 // Temp wires
-wire [127:0] Inv_SB_OUT, Inv_SR_OUT, Inv_MC_OUT, Inv_ARK_OUT;
+wire [127:0] Inv_SB_OUT, Inv_SR_OUT, Inv_MC_OUT, ARK_OUT;
 
 // Inverse Round steps
 Invshift_rows IS_R(state, Inv_SR_OUT);
 InvSubBytes IS_B (Inv_SR_OUT, Inv_SB_OUT);
-AddRoundKey AR_K(Inv_SB_OUT, Inv_ARK_OUT);
-InvMixColumns IM_C(Inv_ARK_OUT, Inv_MC_OUT);
+AddRoundKey AR_K(Inv_SB_OUT, ARK_OUT);
+InvMixColumns IM_C(ARK_OUT, Inv_MC_OUT);
 
 assign InvRoundOut = Inv_MC_OUT;
+
 
 endmodule
